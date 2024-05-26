e8b3982ee4d9b5355d86fc652b4b41e26cf5a3d0
Camila <yourname@example.com>
2024-05-26 17:34:42 +0100

    refactor: Add clear_screen function and improve main function in run.py

--- run.py
+++ run.py
@@ -1,2 +1,13 @@
 import os
 import pyfiglet
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def main():
+    clear_screen()
+    BANNER = pyfiglet.figlet_format("Word Rescue", justify="center", font="doom")
+    print(BANNER)
+
+if __name__ == "__main__":
+    main()
