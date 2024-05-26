8454342b3a136c62dd546b3c6f25d90d02a8cfd2
Camila <yourname@example.com>
2024-05-26 21:46:22 +0100

    refactor: Improve UI layout and add welcome message in run.py

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import os
 import pyfiglet
 from rich.console import Console
-from rich.table import Table
 from rich.padding import Padding
+from rich.prompt import Prompt
+from rich.align import Align
+from rich.text import Text
 
 console = Console()
 
@@ -10,21 +12,30 @@ def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def main():
-    clear_screen()  # Clear the screen
+    clear_screen()
 
     BANNER = pyfiglet.figlet_format("Word Rescue", justify="center", font="doom")
-    padded_banner = Padding(BANNER, (1, 4))  # Add margin around the banner
-    console.print(padded_banner)  # Print the banner with padding
+    padded_banner = Padding(BANNER, (1, 4))
+    console.print(padded_banner)
 
-    table = Table()  # Create a table with a welcome message
-    table.add_column("Welcome to Word Rescue", justify="center", style="magenta")  # Add columns to the table
+    welcome_message = (
+        "The fun and educational word game that challenges you to identify "
+        "famous people from history based on intriguing clues about their lives and achievements. "
+        "Perfect for history lovers, students, and trivia fans, this game lets you learn while you play."
+    )
+    aligned_message = Align(welcome_message, align="center", style="#DAC3F8")
+    padded_message = Padding(aligned_message, (1, 2))
+    console.print(padded_message)
 
-    table.add_row("The fun and educational word game that challenges you to identify "
-                  "famous people from history based on intriguing clues about their lives and achievements. "
-                  "Perfect for history lovers, students, and trivia fans, this game lets you learn while you play.")  # Add rows to the table
+    prompt_message = Align("Please enter your name:", align="center")
+    console.print(prompt_message)
 
-    padded_table = Padding(table, (1, 2))  # Add margin around the table
-    console.print(padded_table)  # Print the table with padding
+    console_width = console.size.width
+    user_name = console.input(f"{' ' * ((console_width - len('Please enter your name:')) // 2)}")
+
+    clear_screen()
+
+    console.print(f"Welcome, {user_name}!", justify="center", style="bold")
 
 if __name__ == "__main__":
     main()
